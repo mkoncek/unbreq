4fb56ec8ae2605eefc5b645e88da0cb6ff7f05f8
Marián Konček <marian.koncek@mailbox.org>
2025-03-12 13:54:28 +0100

    Use epoll

--- src/fanotify.cpp
+++ src/fanotify.cpp
@@ -1,20 +1,20 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/fanotify.h>
+#include <sys/stat.h>
+#include <sys/epoll.h>
+
 #include <algorithm>
 #include <charconv>
 #include <iostream>
 #include <array>
 #include <cstring>
 #include <cerrno>
+#include <climits>
 #include <set>
 #include <string>
-#include <thread>
 #include <format>
 #include <experimental/scope>
-
-#include <fcntl.h>
-#include <unistd.h>
-#include <climits>
-#include <sys/fanotify.h>
-#include <sys/stat.h>
 
 static void checked_close(int fd)
 {
@@ -24,103 +24,93 @@ static void checked_close(int fd)
 	}
 }
 
-void run_fanotify(std::stop_token stop_token, std::string& error, std::set<std::string>& names, int fd, const std::string& mock_root) noexcept
-try
+void run_fanotify(std::set<std::string>& names, int fd, const std::string& mock_root)
 {
 	alignas(fanotify_event_metadata) auto buf = std::array<unsigned char, sizeof(fanotify_event_metadata) * 32>();
 	
-	std::clog << "[INFO] fanotify running..." << "\n";
-	
-	while (not stop_token.stop_requested())
+	auto len = read(fd, std::data(buf), std::size(buf));
+	if (len == -1)
 	{
-		auto len = read(fd, std::data(buf), std::size(buf));
-		if (len == -1)
+		throw std::format("read of {} failed: {}", fd, std::strerror(errno));
+	}
+	
+	if (len <= 0)
+	{
+		return;
+	}
+	
+	auto mount_fd = std::experimental::make_unique_resource_checked(open(mock_root.c_str(), O_DIRECTORY | O_RDONLY), -1, &checked_close);
+	if (mount_fd.get() == -1)
+	{
+		throw std::format("open {} failed: {}", "\".\"", std::strerror(errno));
+	}
+	
+	auto procfd_path = std::array<char, PATH_MAX>();
+	const auto procfd_end = std::ranges::copy(std::string_view("/proc/self/fd/"), std::data(procfd_path)).out;
+	
+	for (fanotify_event_metadata* const metadata = std::bit_cast<fanotify_event_metadata*>(std::data(buf));
+		FAN_EVENT_OK(metadata, len);
+		std::copy(reinterpret_cast<unsigned char*>(FAN_EVENT_NEXT(metadata, len)), std::begin(buf) + len, std::begin(buf)))
+	{
+		if (metadata->vers != FANOTIFY_METADATA_VERSION) [[unlikely]]
 		{
-			throw std::format("read of {} failed: {}", fd, std::strerror(errno));
+			throw std::format("mismatch of fanotify metadata version, expected: {}, found: {}", FANOTIFY_METADATA_VERSION, metadata->vers);
 		}
 		
-		if (len <= 0)
+		auto fid = std::bit_cast<fanotify_event_info_fid*>(metadata + 1);
+		auto file_name = std::string_view();
+		auto file_handle = std::bit_cast<struct file_handle*>(&*fid->handle);
+		
+		if (fid->hdr.info_type == FAN_EVENT_INFO_TYPE_FID)
 		{
-			continue;
+		}
+		else if (fid->hdr.info_type == FAN_EVENT_INFO_TYPE_DFID)
+		{
+		}
+		else if (fid->hdr.info_type == FAN_EVENT_INFO_TYPE_DFID_NAME)
+		{
+			file_name = std::bit_cast<const char*>(&*file_handle->f_handle + file_handle->handle_bytes);
+		}
+		else
+		{
+			throw std::format("received unexpected event info type");
 		}
 		
-		auto mount_fd = std::experimental::make_unique_resource_checked(open(mock_root.c_str(), O_DIRECTORY | O_RDONLY), -1, &checked_close);
-		if (mount_fd.get() == -1)
+		auto event_fd = open_by_handle_at(mount_fd.get(), file_handle, O_RDONLY);
+		if (event_fd == -1)
 		{
-			throw std::format("open {} failed: {}", "\".\"", std::strerror(errno));
-		}
-		
-		auto procfd_path = std::array<char, PATH_MAX>();
-		const auto procfd_end = std::ranges::copy(std::string_view("/proc/self/fd/"), std::data(procfd_path)).out;
-		
-		for (fanotify_event_metadata* const metadata = std::bit_cast<fanotify_event_metadata*>(std::data(buf));
-			not stop_token.stop_requested() && FAN_EVENT_OK(metadata, len);
-			std::copy(reinterpret_cast<unsigned char*>(FAN_EVENT_NEXT(metadata, len)), std::begin(buf) + len, std::begin(buf)))
-		{
-			if (metadata->vers != FANOTIFY_METADATA_VERSION) [[unlikely]]
+			// File handle is no longer valid, we do not care
+			if (errno == ESTALE)
 			{
-				throw std::format("mismatch of fanotify metadata version, expected: {}, found: {}", FANOTIFY_METADATA_VERSION, metadata->vers);
-			}
-			
-			auto fid = std::bit_cast<fanotify_event_info_fid*>(metadata + 1);
-			auto file_name = std::string_view();
-			auto file_handle = std::bit_cast<struct file_handle*>(&*fid->handle);
-			
-			if (fid->hdr.info_type == FAN_EVENT_INFO_TYPE_FID)
-			{
-			}
-			else if (fid->hdr.info_type == FAN_EVENT_INFO_TYPE_DFID)
-			{
-			}
-			else if (fid->hdr.info_type == FAN_EVENT_INFO_TYPE_DFID_NAME)
-			{
-				file_name = std::bit_cast<const char*>(&*file_handle->f_handle + file_handle->handle_bytes);
+				continue;
 			}
 			else
 			{
-				throw std::format("received unexpected event info type");
-			}
-			
-			auto event_fd = open_by_handle_at(mount_fd.get(), file_handle, O_RDONLY);
-			if (event_fd == -1)
-			{
-				// File handle is no longer valid, we do not care
-				if (errno == ESTALE)
-				{
-					continue;
-				}
-				else
-				{
-					throw std::format("open_by_handle_at failed: {}", std::strerror(errno));
-				}
-			}
-			auto event_fd_owner = std::experimental::unique_resource(event_fd, &checked_close);
-			
-			*std::to_chars(procfd_end, std::end(procfd_path), event_fd).ptr = '\0';
-			auto path_buf = std::array<char, PATH_MAX>();
-			auto path_len = readlink(std::data(procfd_path), std::data(path_buf), std::size(path_buf) - 1);
-			if (path_len == -1)
-			{
-				throw std::format("readlink of {} failed: {}", std::string_view(std::data(procfd_path), procfd_end), std::strerror(errno));
-			}
-			
-			auto path = std::string_view(std::data(path_buf), path_len);
-			
-			
-			if (path.starts_with(mock_root))
-			{
-				std::clog << "[DEBUG] file access: " << path << "\n";
-				auto name = std::string();
-				name.reserve(path.size() - mock_root.size() + 1 + file_name.size() + 1);
-				name.append(path.substr(mock_root.size())).append("/").append(file_name);
-				names.insert(name);
+				throw std::format("open_by_handle_at failed: {}", std::strerror(errno));
 			}
 		}
+		auto event_fd_owner = std::experimental::unique_resource(event_fd, &checked_close);
+		
+		*std::to_chars(procfd_end, std::end(procfd_path), event_fd).ptr = '\0';
+		auto path_buf = std::array<char, PATH_MAX>();
+		auto path_len = readlink(std::data(procfd_path), std::data(path_buf), std::size(path_buf) - 1);
+		if (path_len == -1)
+		{
+			throw std::format("readlink of {} failed: {}", std::string_view(std::data(procfd_path), procfd_end), std::strerror(errno));
+		}
+		
+		auto path = std::string_view(std::data(path_buf), path_len);
+		
+		
+		if (path.starts_with(mock_root))
+		{
+			std::clog << "[DEBUG] file access: " << path << "\n";
+			auto name = std::string();
+			name.reserve(path.size() - mock_root.size() + 1 + file_name.size() + 1);
+			name.append(path.substr(mock_root.size())).append("/").append(file_name);
+			names.insert(name);
+		}
 	}
-}
-catch (std::string& ex)
-{
-	error = std::move(ex);
 }
 
 struct Fanotify : std::experimental::unique_resource<int, void(*)(int)>
@@ -149,7 +139,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
 	auto mock_root = std::string(argv[1]);
 	
 	auto fanotify = Fanotify(
-		FAN_CLASS_NOTIF | FAN_CLOEXEC | FAN_REPORT_DFID_NAME /* | FAN_NONBLOCK */,
+		FAN_CLASS_NOTIF | FAN_CLOEXEC | FAN_REPORT_DFID_NAME | FAN_NONBLOCK,
 		O_RDONLY | O_LARGEFILE
 	);
 	
@@ -164,15 +154,54 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] const char** argv)
 		return 1;
 	}
 	
-	auto names = std::set<std::string>();
+	auto epoll_fd = std::experimental::make_unique_resource_checked(epoll_create1(0), -1, &checked_close);
+	if (epoll_fd.get() == -1)
+	{
+		throw std::runtime_error(std::format("failed to create epoll file descriptor: {}", std::strerror(errno)));
+	}
 	
 	{
-		auto error = std::string();
-		auto thread = std::jthread(run_fanotify, std::ref(error), std::ref(names), fanotify.get(), std::cref(mock_root));
-		std::cin.get();
-		if (not error.empty())
+		auto event = epoll_event();
+		event.events = EPOLLIN;
+		
+		event.data.fd = 0;
+		if (epoll_ctl(epoll_fd.get(), EPOLL_CTL_ADD, 0, &event))
 		{
-			std::clog << "an error occured: " << error << "\n";
+			throw std::runtime_error(std::format("failed to add standard input file descriptor to epoll: {}", std::strerror(errno)));
+		}
+		
+		event.data.fd = fanotify.get();
+		if (epoll_ctl(epoll_fd.get(), EPOLL_CTL_ADD, fanotify.get(), &event))
+		{
+			throw std::runtime_error(std::format("failed to add fanotify file descriptor {} to epoll: {}", fanotify.get(), std::strerror(errno)));
+		}
+	}
+	
+	if (fcntl(0, F_SETFL, O_NONBLOCK))
+	{
+		throw std::runtime_error(std::format("failed to set non-blocking mode for the standard input stream: {}", std::strerror(errno)));
+	}
+	
+	auto names = std::set<std::string>();
+	auto events = std::array<epoll_event, 8>();
+	bool keep_running = true;
+	
+	std::clog << "[INFO] fanotify running..." << "\n";
+	
+	while (keep_running)
+	{
+		auto ready_events = epoll_wait(epoll_fd.get(), events.data(), events.size(), -1);
+		for (int i = 0; i != ready_events; ++i)
+		{
+			if (events[i].data.fd == 0)
+			{
+				keep_running = false;
+				break;
+			}
+			else
+			{
+				run_fanotify(names, fanotify.get(), mock_root);
+			}
 		}
 	}
 	
