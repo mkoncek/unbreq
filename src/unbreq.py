413034ce532788c85c7c60a5a1e5124c9adc4396
Marián Konček <marian.koncek@mailbox.org>
2025-09-14 16:41:24 +0200

    Set access time only to files owned by packages listed in BuildRequires

--- src/unbreq.py
+++ src/unbreq.py
@@ -50,11 +50,13 @@ class Unbreq(object):
         self.buildroot = buildroot
         self.showrc_opts = conf
         self.config = buildroot.config
+        
         self.min_time = None
         self.USE_NSPAWN = mockbuild.util.USE_NSPAWN
         self.exclude_accessed_files = [re.compile(r) for r in self.config.get("plugin_conf", {}).get("unbreq_opts", {}).get("exclude_accessed_files", [])]
         self.accessed_files = AtimeDict()
         self.mount_options = None
+        self.buildrequires = None
 
         # TODO handle different package managers
         # self.buildroot.pkg_manager.name
@@ -63,43 +65,36 @@ class Unbreq(object):
         plugins.add_hook("postbuild", self._PostBuildHook)
 
     @traceLog()
-    def resolve_buildrequires(self):
-        if self.USE_NSPAWN:
-            chroot_command = ["/usr/bin/systemd-nspawn", "--quiet", "--pipe", "-D", self.buildroot.bootstrap_buildroot.rootdir, "--bind", self.buildroot.rootdir]
+    def get_files(self, packages):
+        if len(packages) == 0:
+            return list()
+        process = subprocess.run(self.chroot_command + ["/usr/bin/rpm", "--root", self.buildroot.rootdir, "-ql"] + packages,
+            stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True
+        )
+        if process.returncode != 0:
+            raise RuntimeError("process {} returned {}: {}".format(
+                process.args, process.returncode, process.stderr.rstrip()
+            ))
         else:
-            chroot_command = ["/usr/bin/chroot", self.buildroot.bootstrap_buildroot.rootdir]
-        chroot_dnf_command = chroot_command + ["/usr/bin/dnf", "--installroot", self.buildroot.rootdir]
-        srpm_dir = self.buildroot.rootdir + os.path.join(self.buildroot.builddir, "SRPMS")
-
-        def get_files(packages):
-            if len(packages) == 0:
-                return list()
-            process = subprocess.run(chroot_command + ["/usr/bin/rpm", "--root", self.buildroot.rootdir, "-ql"] + packages,
-                stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.PIPE,
+            return process.stdout.splitlines()
+    
+    @traceLog()
+    def resolve_buildrequires(self):
+        br_providers = dict()
+        rev_br_providers = dict()
+        for br in self.buildrequires:
+            process = subprocess.run(
+                self.chroot_dnf_command + ["repoquery", "--installed", "--whatprovides", br],
+                stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True
             )
             if process.returncode != 0:
                 raise RuntimeError("process {} returned {}: {}".format(
-                    process.args, process.returncode, process.stderr.decode("utf-8").rstrip()
+                    process.args, process.returncode, process.stderr.strip()
                 ))
-            else:
-                return process.stdout.decode("ascii").splitlines()
-
-        br_providers = dict()
-        rev_br_providers = dict()
-        for srpm in os.scandir(srpm_dir):
-            for br in get_buildrequires(srpm.path):
-                process = subprocess.run(
-                    chroot_dnf_command + ["repoquery", "--installed", "--whatprovides", br],
-                    stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.PIPE,
-                )
-                if process.returncode != 0:
-                    raise RuntimeError("process {} returned {}: {}".format(
-                        process.args, process.returncode, process.stderr.decode("utf-8").strip()
-                    ))
-                br_providers_br = process.stdout.decode("ascii").splitlines()
-                br_providers[br] = br_providers_br
-                for provider in br_providers_br:
-                    rev_br_providers.setdefault(provider, list()).append(br)
+            br_providers_br = process.stdout.splitlines()
+            br_providers[br] = br_providers_br
+            for provider in br_providers_br:
+                rev_br_providers.setdefault(provider, list()).append(br)
         # attempt to resolve providers so that each BR is provided by only one provider
         sorted_br_providers = sorted(br_providers, key = lambda k: len(br_providers[k]))
         if len(sorted_br_providers) != 0 and len(sorted_br_providers[-1]) > 1:
@@ -119,15 +114,15 @@ class Unbreq(object):
 
         brs_can_be_removed = list()
         for br, providers in br_providers.items():
-            process = subprocess.run(chroot_dnf_command + ["--assumeno", "--setopt", "protected_packages=", "remove"] + [v for vs in brs_can_be_removed for v in vs[1]] + providers,
-                stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.PIPE,
+            process = subprocess.run(self.chroot_dnf_command + ["--assumeno", "--setopt", "protected_packages=", "remove"] + [v for vs in brs_can_be_removed for v in vs[1]] + providers,
+                stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True
             )
             if process.returncode != 1:
                 raise RuntimeError("process {} returned {}: {}".format(
-                    process.args, process.returncode, process.stderr.decode("utf-8").rstrip()
+                    process.args, process.returncode, process.stderr.rstrip()
                 ))
             removed_packages = list()
-            for line in process.stdout.decode("ascii").splitlines():
+            for line in process.stdout.splitlines():
                 if not line.startswith(" "):
                     continue
                 nvr = line.split()
@@ -136,7 +131,7 @@ class Unbreq(object):
                 nvr = nvr[0] + "-" + nvr[2] + "." + nvr[1]
                 removed_packages.append(nvr)
             can_be_removed = True
-            for path in get_files(removed_packages):
+            for path in self.get_files(removed_packages):
                 path = self.buildroot.rootdir + path
                 try:
                     atime = self.accessed_files[path]
@@ -150,6 +145,7 @@ class Unbreq(object):
                             skip = True
                             break
                     else:
+                        getLog().info("unbreq plugin: cannot remove %s because file %s was accessed", br, short_path)
                         can_be_removed = False
                         break
             if can_be_removed:
@@ -159,33 +155,66 @@ class Unbreq(object):
             getLog().warning("unbreq plugin: the following BuildRequires were not used:\n\t%s", "\n\t".join(brs))
 
     @traceLog()
+    def set_am_time(self):
+        process = subprocess.run(self.chroot_dnf_command + ["--assumeno", "--setopt", "protected_packages=", "remove"] + self.buildrequires,
+            stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True,
+        )
+        if process.returncode != 1:
+            raise RuntimeError("process {} returned {}: {}".format(
+                process.args, process.returncode, process.stderr.rstrip()
+            ))
+        nvrs = []
+        for line in process.stdout.splitlines():
+            if not line.startswith(" "):
+                continue
+            nvr = line.split()
+            if len(nvr) != 6:
+                continue
+            nvrs.append(nvr[0] + "-" + nvr[2] + "." + nvr[1])
+        
+        for filename in set(self.get_files(nvrs)):
+            try:
+                os.utime(self.buildroot.rootdir + filename, (0, 0))
+            except FileNotFoundError:
+                pass
+
+    @traceLog()
     def _PreBuildHook(self):
         getLog().info("enabled unbreq plugin (prebuild)")
         
-        try:
-            mount_options_process = subprocess.run(["findmnt", "-n", "-o", "OPTIONS", "--target", self.buildroot.rootdir], check = True, text = True, capture_output = True)
-            if mount_options_process:
-                self.mount_options = mount_options_process.stdout.rstrip().split(",")
-        except FileNotFoundError:
-            pass
+        if self.USE_NSPAWN:
+            self.chroot_command = ["/usr/bin/systemd-nspawn", "--quiet", "--pipe", "-D", self.buildroot.bootstrap_buildroot.rootdir, "--bind", self.buildroot.rootdir]
+        else:
+            self.chroot_command = ["/usr/bin/chroot", self.buildroot.bootstrap_buildroot.rootdir]
+        self.chroot_dnf_command = self.chroot_command + ["/usr/bin/dnf", "--installroot", self.buildroot.rootdir]
+        self.srpm_dir = self.buildroot.rootdir + os.path.join(self.buildroot.builddir, "SRPMS")
+        
+        self.buildrequires = set()
+        for srpm in os.scandir(self.srpm_dir):
+            for br in get_buildrequires(srpm.path):
+                self.buildrequires.add(br)
+        self.buildrequires = sorted(set(self.buildrequires))
         
         # NOTE maybe find a better example file to touch to get an atime?
         path = os.path.join(self.buildroot.rootdir, "dev", "null")
         subprocess.run(["touch", path], check = True)
         self.min_time = os.stat(path).st_atime
         
+        try:
+            mount_options_process = subprocess.run(["findmnt", "-n", "-o", "OPTIONS", "--target", self.buildroot.rootdir], stdin = subprocess.DEVNULL, stdout = subprocess.PIPE, stderr = subprocess.PIPE, text = True)
+            if mount_options_process:
+                self.mount_options = mount_options_process.stdout.rstrip().split(",")
+        except FileNotFoundError:
+            pass
+        
         if "relatime" in self.mount_options:
             getLog().info("unbreq plugin: detected 'relatime' mount option, going to set access times of files under %s to 0", self.buildroot.rootdir)
-            for dirname, dirnames, filenames in os.walk(self.buildroot.rootdir):
-                # Ignore /dev/
-                if dirname == self.buildroot.rootdir:
-                    dirnames.remove("dev")
-                try:
-                    os.utime(dirname, (0, 0))
-                    for filename in filenames:
-                        os.utime(os.path.join(dirname, filename), (0, 0))
-                except FileNotFoundError:
-                    pass
+            if self.USE_NSPAWN:
+                self.set_am_time()
+            else:
+                with mockbuild.mounts.BindMountPoint(self.buildroot.rootdir,
+                    self.buildroot.bootstrap_buildroot.make_chroot_path(self.buildroot.rootdir)).having_mounted():
+                    self.set_am_time()
 
     @traceLog()
     def _PostBuildHook(self):
